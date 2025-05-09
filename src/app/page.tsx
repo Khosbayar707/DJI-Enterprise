9a772b235f206967dc25656a5c959e5a53b023f6
Khosbayar707 <torutora0415@gmail.com>
2025-05-09 10:42:36 +0800

    header footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Header from "./_component/Header";
+import Footer from "./_component/Footer";
 
 export default function Home() {
-  return <div>hi</div>;
+  return <div></div>;
 }
