f24330db6571a8cd01d2a0f9eadd40b18f627697
glpzghoo <my.as.glpzghoo@gmail.com>
2025-05-31 21:28:45 +0800

    preview init

--- src/app/preview/[id]/page.tsx
+++ src/app/preview/[id]/page.tsx
@@ -2,13 +2,52 @@
 import HeroSection from "../_components/HeroSection";
 import FeaturesSection from "../_components/FeaturesSection";
 import DetailSection from "../_components/DetailSection";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { CustomDroneClient } from "@/lib/types";
+import axios from "axios";
+import LoadingText from "@/app/_component/LoadingText";
 
 export default function HomePage() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState(true);
+  const [drone, setDrone] = useState<CustomDroneClient>();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/api/client/products/drone?id=${id}`);
+        if (res.data.success) {
+          setDrone(res.data.data.drone);
+        }
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <main className="bg-black text-white">
-      <HeroSection />
-      <FeaturesSection />
-      <DetailSection />
+      {loading ? (
+        <div className=" flex justify-center min-h-screen items-center">
+          {" "}
+          <LoadingText />
+        </div>
+      ) : drone ? (
+        <>
+          <HeroSection drone={drone} />
+          <FeaturesSection drone={drone} />
+          {drone.specs.length > 0 ? (
+            drone.specs.map((spec) => <DetailSection spec={spec} />)
+          ) : (
+            <div>Эд ангийн showcase алга</div>
+          )}
+        </>
+      ) : (
+        <div>Холбоос буруу</div>
+      )}
     </main>
   );
 }
--- src/app/preview/_components/HeroSection.tsx
+++ src/app/preview/_components/HeroSection.tsx
@@ -1,20 +1,27 @@
+"use client";
+import { CustomDroneClient } from "@/lib/types";
 import { motion } from "framer-motion";
+import { useMemo } from "react";
 
-export default function HeroSection() {
+type Props = {
+  drone: CustomDroneClient;
+};
+export default function HeroSection({ drone }: Props) {
   return (
     <section className="relative h-screen w-full overflow-hidden">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute inset-0 h-full w-full object-cover"
-      >
-        <source
-          src="https://www-cdn.djiits.com/reactor/assets/_next/static/videos/c75a5a56-9455-4783-9ea5-eff9835beee5.mp4"
-          type="video/mp4"
-        />
-      </video>
+      {drone.videos.length > 0 ? (
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute inset-0 h-full w-full object-cover"
+        >
+          <source src={drone.videos[0].url} type="video/mp4" />
+        </video>
+      ) : (
+        <div className="absolute inset-0 h-full w-full object-cover"></div>
+      )}
       <div className="absolute bottom-0 left-0 right-0 h-40 bg-gradient-to-t from-black to-transparent"></div>
       <div className="relative z-10 flex h-full w-full flex-col items-center justify-center bg-black/50">
         <motion.h1
@@ -23,7 +30,7 @@ export default function HeroSection() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          DJI Mavic 3 Enterprise
+          {drone.name}
         </motion.h1>
         <motion.p
           className="mt-4 text-lg md:text-xl text-gray-300"
@@ -31,7 +38,7 @@ export default function HeroSection() {
           animate={{ opacity: 1 }}
           transition={{ delay: 0.5, duration: 1 }}
         >
-          Мэргэжлийн зориулалттай хүчирхэг нисгэгчгүй нисэх төхөөрөмж
+          {drone.PreviewDescription}
         </motion.p>
       </div>
     </section>
