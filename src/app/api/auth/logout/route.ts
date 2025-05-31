00c77e87cb5a74d11cda90ea172c392cc3160079
glpzghoo <my.as.glpzghoo@gmail.com>
2025-05-31 13:47:26 +0800

    contact request - client

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -2,12 +2,12 @@ import { CustomResponse } from "@/lib/next-responses";
 import { NextResponse } from "next/server";
 
 export async function GET(req: NextResponse) {
-  const response = CustomResponse(
-    true,
-    "LOGOUT_SUCCESS",
-    "Хэрэглэгч амжилттай гарлаа!",
-    {}
-  );
+  const response = NextResponse.json({
+    success: true,
+    code: "LOGOUT_SUCCESS",
+    message: "Хэрэглэгч амжилттай гарлаа!",
+  });
+
   response.cookies.delete("accessToken");
   response.cookies.delete("refreshToken");
   return response;
