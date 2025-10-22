da6a60c57005f47a5df7e3e78926e8e6f3f9cb30
Khosbayar707 <torutora0415@gmail.com>
2025-10-22 12:32:53 +0800

    2

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,7 @@
 export default function Head() {
   return (
     <>
-      <meta
-        name="google-site-verification"
-        content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
-      />
+      <meta name="google-site-verification" content="6oy8e-7nRgqscd_PfMPNYJ_IGtlh3vC5a16JQddB3bo" />
       <link rel="canonical" href="https://www.djigeo.mn" />
     </>
   );
